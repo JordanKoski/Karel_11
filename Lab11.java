454a49d310b26d21ac32a5a9f2152d1982522ed1
JordanKoski <jordan_koski@asl.oeg>
2014-12-10 12:06:59 +0000

    Lab11

--- Lab11.java
+++ Lab11.java
@@ -1,17 +1,36 @@
 /**
  * An algorithm helps an Athlete solve a maze, perhaps by keeping one hand against a wall.
  * 
- * @author <...>
- * @version <...>
+ * @author <Jordan Koski>
+ * @version <8th December>
  *
  */
 
-import edu.fcps.karel2.Display; 
+import edu.fcps.karel2.Display;
+import edu.fcps.Digit;
 
 public class Lab11 {
 	 
 	 public static void main(String[] args) {
+    
+    //Display.openWorld("maps/.map");
+         Display.setSize(42, 37);
+         Display.setSpeed(10);
+         
+         Digit first = new One (5,10);
+         Digit second = new One (15,10);
+         Digit third = new Nine (25,10);
+         Digit fourth = new Three (35,10);
+         
+         first.display();
+         second.display();
+         third.display();
+         fourth.display();
+
+      
+
 	
-	 }
+	 
+    }
 	 
  }
